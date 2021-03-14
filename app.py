eecdbb9d6952b418e7b52461b186531bb0669065
Pratik K <pratik.k98@yahoo.com>
2021-03-14 14:07:25 +0400

    [Addition] streamlit app boilerplate

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def main():
+    st.title("Persimmon")
+
+    """
+    > Optimizing physician distribution between recurring elective surgeries and an ongoing pandemic
+    """
+
+
+
+if __name__ == "__main__":
+    main()
